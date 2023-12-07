6d4ad5a6f464eb889de58e47249a8509d43c9504
Tanmay Shil <101269992+TanmayShil@users.noreply.github.com>
2023-12-07 12:23:47 +0530

    Update Form.jsx

--- dropstack/frontend/src/components/form/Form.jsx
+++ dropstack/frontend/src/components/form/Form.jsx
@@ -161,7 +161,7 @@ const Form = () => {
           <div className="container-fluid justify-content-center px-5">
             {/* <!-- Navbar brand --> */}
             <a className="navbar-brand me-2" href="#!">
-              <img src="images/a1-logo.webp" height="100" width="100" alt="MDB Logo" loading="lazy"
+              <img src="images/xyz.jpg" height="100" width="100" alt="MDB Logo" loading="lazy"
                 style={{ marginTop: "-1px" }} />
             </a>
           </div>
@@ -409,120 +409,6 @@ const Form = () => {
           </form>
         </div>
       </section>
-      <footer className="text-center text-lg-start">
-        {/* <!-- Grid container --> */}
-        <div className="container p-5">
-          {/* <!--Grid row--> */}
-          <div className="row">
-            {/* <!--Grid column--> */}
-            <div className="col-lg-5 col-md-6 col-12 mb-4 mb-md-0">
-
-
-              <ul className="list-unstyled  menu-footer">
-                <li>
-                  <a href="#!" className="text-dark">Home</a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">Case Studies</a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">Process</a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">Partner With Us</a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">Join The Team</a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">Contact Us</a>
-                </li>
-              </ul>
-
-              <div className="menu-footer-btm">
-                <ul className="list-unstyled d-flex">
-                  <li><span>GSTIN: 19AAKCA7063N1Z1</span></li>
-                  <li><span>CIN: U72200WB2012PTC183279</span></li>
-                </ul>
-              </div>
-            </div>
-
-
-            {/* <!--Grid column--> */}
-
-            {/* <!--Grid column--> */}
-            <div className="col-lg-4 col-md-6 col-12  mb-md-0 blog-div">
-              <h5 className="mb-4">Blog Post</h5>
-
-              <ul className="list-unstyled">
-                <li className="mb-4">
-                  <a href="#!" className="text-dark">8 Crucial Factors to Evaluate in a Creative Agency’s Process When Choosing
-                    Your Partner</a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">Take the Leap, Scale Worryless: Embrace Flexibility with On-Demand Design
-                    Services</a>
-                </li>
-
-              </ul>
-            </div>
-            {/* <!--Grid column--> */}
-
-            {/* <!--Grid column--> */}
-            <div className="col-lg-3 col-md-6 col-12 mb-4 mb-md-0">
-              <a href="#!">
-                <img src="images/a1ft-footer.png" alt="logo-footer" width="219.78" height="75.33" />
-              </a>
-              <div className="mt-4">
-                <span className="copyright">© Copyright 2023</span>
-              </div>
-
-              <div className="policy-list mb-4 mt-3">
-                <ul className="list-unstyled mb-0 d-flex ">
-                  <li><a href="#!">Privacy Policy</a></li>
-                  <li><a href="#!">Cookie Policy</a></li>
-                </ul>
-              </div>
-              <div className="social-icons mt-4">
-                <ul className="list-unstyled mb-0 d-flex">
-                  <li>
-                    <a href="#!" className="text-dark"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"
-                      viewBox="0 0 26 26">
-                      <path fill="white"
-                        d="M14 13.5h2.5l1-4H14v-2c0-1.03 0-2 2-2h1.5V2.14c-.326-.043-1.557-.14-2.857-.14C11.928 2 10 3.657 10 6.7v2.8H7v4h3V22h4v-8.5Z" />
-                    </svg></a>
-                  </li>
-                  <li>
-                    <a href="#!" className="text-dark"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"
-                      viewBox="0 0 26 26">
-                      <path fill="white"
-                        d="M12.001 9a3 3 0 1 0 0 6a3 3 0 0 0 0-6Zm0-2a5 5 0 1 1 0 10a5 5 0 0 1 0-10Zm6.5-.25a1.25 1.25 0 0 1-2.5 0a1.25 1.25 0 0 1 2.5 0ZM12.001 4c-2.474 0-2.878.007-4.029.058c-.784.037-1.31.142-1.798.332a2.886 2.886 0 0 0-1.08.703a2.89 2.89 0 0 0-.704 1.08c-.19.49-.295 1.015-.331 1.798C4.007 9.075 4 9.461 4 12c0 2.475.007 2.878.058 4.029c.037.783.142 1.31.331 1.797c.17.435.37.748.702 1.08c.337.336.65.537 1.08.703c.494.191 1.02.297 1.8.333C9.075 19.994 9.461 20 12 20c2.475 0 2.878-.007 4.029-.058c.782-.037 1.308-.142 1.797-.331a2.91 2.91 0 0 0 1.08-.703c.337-.336.538-.649.704-1.08c.19-.492.296-1.018.332-1.8c.052-1.103.058-1.49.058-4.028c0-2.474-.007-2.878-.058-4.029c-.037-.782-.143-1.31-.332-1.798a2.912 2.912 0 0 0-.703-1.08a2.884 2.884 0 0 0-1.08-.704c-.49-.19-1.016-.295-1.798-.331C14.926 4.006 14.54 4 12 4Zm0-2c2.717 0 3.056.01 4.123.06c1.064.05 1.79.217 2.427.465c.66.254 1.216.598 1.772 1.153a4.908 4.908 0 0 1 1.153 1.772c.247.637.415 1.363.465 2.428c.047 1.066.06 1.405.06 4.122c0 2.717-.01 3.056-.06 4.122c-.05 1.065-.218 1.79-.465 2.428a4.884 4.884 0 0 1-1.153 1.772a4.915 4.915 0 0 1-1.772 1.153c-.637.247-1.363.415-2.427.465c-1.067.047-1.406.06-4.123.06c-2.717 0-3.056-.01-4.123-.06c-1.064-.05-1.789-.218-2.427-.465a4.89 4.89 0 0 1-1.772-1.153a4.905 4.905 0 0 1-1.153-1.772c-.248-.637-.415-1.363-.465-2.428c-.048-1.066-.06-1.405-.06-4.122c0-2.717.01-3.056.06-4.122c.05-1.066.217-1.79.465-2.428a4.88 4.88 0 0 1 1.153-1.772a4.897 4.897 0 0 1 1.772-1.153c.637-.248 1.362-.415 2.427-.465C8.945 2.013 9.284 2 12.001 2Z" />
-                    </svg></a>
-                  </li>
-                  <li>
-                    <a href="#!" className="text-dark"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"
-                      viewBox="0 0 26 26">
-                      <path fill="white" fillRule="evenodd"
-                        d="M9.429 8.969h3.714v1.85c.535-1.064 1.907-2.02 3.968-2.02c3.951 0 4.889 2.118 4.889 6.004V22h-4v-6.312c0-2.213-.535-3.461-1.897-3.461c-1.889 0-2.674 1.345-2.674 3.46V22h-4V8.969ZM2.57 21.83h4V8.799h-4V21.83ZM7.143 4.55a2.53 2.53 0 0 1-.753 1.802a2.573 2.573 0 0 1-1.82.748a2.59 2.59 0 0 1-1.818-.747A2.548 2.548 0 0 1 2 4.55c0-.677.27-1.325.753-1.803A2.583 2.583 0 0 1 4.571 2c.682 0 1.336.269 1.819.747c.482.478.753 1.126.753 1.803Z"
-                        clipRule="evenodd" />
-                    </svg></a>
-                  </li>
-
-                </ul>
-              </div>
-
-            </div>
-            {/* <!--Grid column--> */}
-
-
-          </div>
-          {/* <!--Grid row--> */}
-        </div>
-        {/* <!-- Grid container --> */}
-
-
-      </footer>
-
     </div>
   );
 
